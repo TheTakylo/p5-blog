688a69af5549713f30a9bfd317129eca37b03977
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-01-13 14:47:16 +0100

    List, validate & remove blog post comment

--- app/Repository/CommentRepository.php
+++ app/Repository/CommentRepository.php
@@ -22,6 +22,34 @@ class CommentRepository extends AbstractRepository
         return ($pageNumber - 1) * self::getMaxElements();
     }
 
+    public function findAllWithPosts($validated = 1)
+    {
+
+        $query = "SELECT posts.slug, posts.title, comments.*
+        FROM comments 
+        LEFT JOIN posts ON posts.id = comments.post_id
+        WHERE comments.validated = :validated
+        GROUP BY comments.id 
+        ORDER BY comments.created_at DESC";
+
+        $query = $this->db->prepare($query);
+
+        $query->execute(['validated' => $validated]);
+
+        return $this->hydrateEntities($query->fetchAll());
+
+    }
+
+    public function validate($id): bool
+    {
+        $query = "UPDATE comments SET validated=1 WHERE id=:id";
+        $query = $this->db->prepare($query);
+
+        return $query->execute([
+            'id' => $id,
+        ]);
+    }
+
     static function getMaxElements(): int
     {
         return (int)self::$pagination['maxElements'];
