840983af3b2e513efc008b49a2765cbfed688c8d
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-02-11 18:58:52 +0100

    Fix errors in prod

--- framework/Database/Database.php
+++ framework/Database/Database.php
@@ -10,10 +10,9 @@ class Database
 
     public function __construct(array $config)
     {
-        $this->database = new \PDO("mysql:host={$config['host']};dbname={$config['database']}", $config['user'], $config['password']);
+        $this->database = new \PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", $config['user'], $config['password']);
         $this->database->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
         $this->database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->database->setAttribute(\PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
     }
 
     public function getConnection(): \PDO
